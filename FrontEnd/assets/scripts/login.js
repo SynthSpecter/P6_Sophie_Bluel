99dc774d6b5b2125c229205f7f18725af0ce4068
Kevin T. Leroux <kev.tl63@gmail.com>
2023-07-24 16:50:49 +0200

    test new login

--- FrontEnd/assets/scripts/login.js
+++ FrontEnd/assets/scripts/login.js
@@ -1,45 +1,39 @@
-// On stocke la valeur précise du token de connexion
-const token = localStorage.getItem(
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4'
-);
-const loginForm = document.getElementById('loginForm');
+function login() {
+    const loginForm = document.getElementById('loginForm');
 
-// J'empêche le comportement par défaut du bouton "se connecter"
-function loginFormSubmit(event) {
-  event.preventDefault()
+    // Je place un écouteur d'événement pour le clic sur le bouton "se connecter" tout en empêchant son comportement par défaut de se produire
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
 
-// Je place un écouteur d'événement pour le clic sur le bouton "se connecter"
-loginForm.addEventListener('submit', loginFormSubmit)
+        // Je récupère les valeurs des champs d'identification
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
 
-  // Je récupère les valeurs des champs d'identification
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
+        // J'envoie les informations d'identification à l'API puis traitement des réponses
+        try {
+            const response = await fetch("http://localhost:5678/api/users/login", {
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email, password }),
+            })
 
-  // J'envoie les informations d'identification à l'API puis traitement des réponses
-  fetch('http://localhost:5678/api/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`,
-    },
-    body: JSON.stringify({ email, password }),
-  })
-    .then((data) => {
-      // (Si les id et pw sont corrects, ça stocke le token)
-      try {
-        if (data && data.token) {
-          localStorage.setItem('token', data.token)
-          window.location.href = 'index.html'
-        } else {
-          throw new Error("Token manquant dans la réponse de l'API")
+            const token = await response.json();
+            localStorage.setItem("token", JSON.stringify(token));
+
+            if (response.ok) {
+                window.location = "../../index.html";
+            } else {
+                error.textContent = "Nom d'utilisateur ou mot de passe incorrect";
+            }
+
+        } catch (error) {
+            alert("problème de connexion au serveur");
         }
-      } catch (error) {
-        console.error('Erreur lors de la requête API:', error)
-        alert('Une erreur est survenue lors de la connexion')
-      }
-    })
-    .catch((error) => {
-      console.error('Erreur lors de la requête API:', error)
-      alert('Une erreur est survenue lors de la connexion')
+
     })
 }
+
+login()
