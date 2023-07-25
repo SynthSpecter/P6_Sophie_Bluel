d99a27792d0d5287ea109867a710233c86b1f85a
Kevin T. Leroux <kev.tl63@gmail.com>
2023-07-25 17:20:36 +0200

    in progress -- recharger la galerie via l'API

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,6 +1,5 @@
 async function getGalleryData() {
   try {
-    // Effectuer une requête GET à l'API pour récupérer les éléments de la galerie
     const response = await fetch('http://localhost:5678/api/works', {
       'method': 'GET',
       'Content-Type': 'application/json',
@@ -12,7 +11,6 @@ async function getGalleryData() {
     
     const data = await response.json();
     
-    // Appeler la fonction pour afficher les éléments de la galerie dans la page HTML
     displayGallery(data);
   } catch (error) {
     console.error('Une erreur est survenue lors de la récupération des éléments de la galerie :', error);
@@ -22,10 +20,8 @@ async function getGalleryData() {
 function displayGallery(data) {
   const galleryContainer = document.querySelector('.gallery');
 
-  // Vider le contenu de la galerie actuelle
   galleryContainer.innerHTML = '';
 
-  // Parcourir les éléments de la galerie et les afficher dans la page HTML
   data.forEach(item => {
     const figure = document.createElement('figure');
     const image = document.createElement('img');
@@ -42,5 +38,4 @@ function displayGallery(data) {
   });
 }
 
-// Appeler la fonction pour récupérer et afficher les éléments de la galerie au chargement de la page
 getGalleryData();
