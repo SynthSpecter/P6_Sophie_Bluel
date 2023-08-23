af74621080dad5e95d75938cb7a921787cc49bbc
Kevin T. Leroux <kev.tl63@gmail.com>
2023-08-23 10:22:22 +0200

    wip -- refactoring et modal second part -- ajout

--- FrontEnd/assets/scripts/modal.js
+++ FrontEnd/assets/scripts/modal.js
@@ -3,21 +3,87 @@ const backButton = document.querySelector('.back-button');
 const closeButton = document.getElementById('closeModalButton');
 const modalContent = document.querySelector('.modal-content');
 const addPhotoSection = document.querySelector('.add-photo-section');
+const addPhotoForm = document.getElementById('addPhotoForm');
 const addPhotoFormButton = document.getElementById('addPhotoFormButton');
 const closeModalButton = document.getElementById('closeModalButton');
 const galleryMini = document.querySelector('.gallery-mini');
+const fileInput = document.getElementById('imageURL');
+const fileInputLabel = document.getElementById('file-input-label');
+const uploadModalButton = document.getElementById('uploadModalButton');
+const imageCategorySelect = document.getElementById('imageCategory');
+const addFinalPhoto = document.getElementById('addFinalPhotoButton');
 
 addPhotoButton.addEventListener('click', openModal)
 closeButton.addEventListener('click', closeModal);
-
 addPhotoFormButton.addEventListener('click', () => {
   addPhotoSection.classList.add('slide-right');
+  // changer le display de none en block
   backButton.style.display = 'block';
 });
-
 backButton.addEventListener('click', () => {
   addPhotoSection.classList.remove('slide-right');
   backButton.style.display = 'none';
+});
+uploadModalButton.addEventListener('click', () => {
+   fileInput.click();
+});
+addFinalPhoto.addEventListener('click', (event) => {
+  addPhoto(event);
+});
+fileInput.addEventListener('change', () => {
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(event) {
+      const previewImage = document.createElement('img');
+      previewImage.src = event.target.result;
+      previewImage.alt = 'Image sélectionnée';
+      previewImage.classList.add('preview-image');
+
+      const uploadZone = document.querySelector('.uploadZone');
+      uploadZone.innerHTML = '';
+      uploadZone.appendChild(previewImage);
+    }
+    reader.readAsDataURL(file);
+  } else {
+    const uploadZone = document.querySelector('.uploadZone');
+    uploadZone.innerHTML = `
+      <img src="../FrontEnd/assets/icons/download.png" alt="image de téléchargement">
+      <button id="uploadModalButton">+ Ajouter une photo</button>
+      <span class="file-restrictions">jpg, png, : 4mo max</span>
+    `;
+
+    fileInputLabel.textContent = 'Ajout photo';
+  }
+});
+addPhotoForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const selectedImageURL  = document.getElementById('imageURL').value;
+  const imageTitle = document.getElementById('imageTitle').value;
+  const imageCategory = document.getElementById('imageCategory').value;
+
+  try {
+    const response = await fetch('http://localhost:5678/api/works', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        imageUrl: selectedImageURL,
+        title: imageTitle,
+        category: imageCategory,
+      }),
+    });
+
+    if (response.ok) {
+    } else {
+      console.error('Erreur lors de l\'ajout de l\'image');
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 function openModal() {
@@ -25,11 +91,6 @@ function openModal() {
   closeModalButton.style.display = 'block';
   galleryMini.style.display = 'flex';
   populateGalleryMini();
-}
- 
-function closeModal() {
-  modal.style.display = 'none';
-  galleryMini.style.display = 'none';
 }
 
 async function populateGalleryMini() {
@@ -127,12 +188,31 @@ function deleteGallery() {
   // Code pour supprimer la galerie
 }
 
-const addPhotoForm = document.getElementById('addPhotoForm');
+async function populateImageCategories() {
+  try {
+    const response = await fetch('http://localhost:5678/api/categories');
+    if (!response.ok) {
+      throw new Error('Erreur lors de la récupération des catégories');
+    }
+    const categories = await response.json();
 
-addPhotoForm.addEventListener('submit', async (event) => {
+    categories.forEach(category => {
+      const option = document.createElement('option');
+      option.value = category.id;
+      option.textContent = category.name;
+      imageCategorySelect.appendChild(option);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+populateImageCategories();
+
+async function addPhoto(event) {
   event.preventDefault();
 
-  const imageURL = document.getElementById('imageURL').value;
+  const selectedImageURL = document.getElementById('imageURL').value;
   const imageTitle = document.getElementById('imageTitle').value;
   const imageCategory = document.getElementById('imageCategory').value;
 
@@ -144,17 +224,40 @@ addPhotoForm.addEventListener('submit', async (event) => {
         'Authorization': `Bearer ${token}`,
       },
       body: JSON.stringify({
-        imageUrl: imageURL,
+        imageUrl: selectedImageURL ,
         title: imageTitle,
         category: imageCategory,
       }),
     });
 
     if (response.ok) {
+      closeModal();
+      populateGalleryMini();
     } else {
       console.error('Erreur lors de l\'ajout de l\'image');
     }
   } catch (error) {
     console.error(error.message);
   }
-});
+}
+
+// function resetUploadZone() {
+//   const uploadZone = document.querySelector('.uploadZone');
+//   uploadZone.innerHTML = `
+//     <img src="../FrontEnd/assets/icons/download.png" alt="image de téléchargement">
+//     <input type="file" id="imageURL" name="imageURL" accept=".jpg, .png" style="display: none">
+//     <button id="uploadModalButton">+ Ajouter une photo</button>
+//     <span class="file-restrictions">jpg, png, : 4mo max</span>
+//   `;
+//   fileInputLabel.textContent = 'Ajout photo';
+// }
+
+// closeModalButton.addEventListener('click', () => {
+//   closeModal();
+//   resetUploadZone();
+// });
+
+function closeModal() {
+  modal.style.display = 'none';
+  galleryMini.style.display = 'none';
+}
